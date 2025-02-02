56155827bf11aba49ec62be18e0f694e4dd81feb
Carlo Luis De Guzman <carlo@Carlos-MacBook-Pro.local>
2025-02-01 19:57:11 -0500

    Bumped up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.splashapplication"
         minSdk = 34
         targetSdk = 34
-        versionCode = 2
-        versionName = "Version 1"
+        versionCode = 5
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
